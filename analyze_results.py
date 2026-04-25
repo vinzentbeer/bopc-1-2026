a4aa74a67201e6981bb1fab2479905eff4a1bcc1
Vinz <vinzent.beer@gmail.com>
2026-04-25 20:13:05 +0200

    add runs from cluster for 2.3 and 2.4 experiment sets, extend analyze results with nproc specific data

--- analyze_results.py
+++ analyze_results.py
@@ -38,6 +38,41 @@ def process_data(filename):
 
     return pd.DataFrame(results)
 
+
+
+def process_patch_experiment(filename):
+    data = []
+    with open(filename, "r") as f:
+        for line in f:
+            size, patch, nprocs, time = line.strip().split(";")
+            data.append((int(size), int(patch), int(nprocs), float(time)))
+
+    df = pd.DataFrame(data, columns=["size", "patch", "nprocs", "time"])
+
+    grouped = (
+        df.groupby(["size", "nprocs", "patch"])["time"]
+        .mean()
+        .reset_index()
+        .rename(columns={"time": "mean_time"})
+        .sort_values(["size", "nprocs", "patch"])
+    )
+
+    return df, grouped
+
+
+#2.3 and 2.4 data: 
+
+df_23, grouped_2_3 = process_patch_experiment("output_exp23_5116564.dat")
+df_24, grouped_2_4 = process_patch_experiment("output_exp24_5116759.dat")
+
+print(df_23.head() )
+print(df_24.head())
+#save to csv 
+grouped_2_3.to_csv("results_patch_2_3.csv", index=False)
+grouped_2_4.to_csv("results_patch_2_4.csv", index=False)    
+
+
+"""
 # Load both datasets
 df_cb = process_data("output_exp22_1_5116562.dat")
 df_cs = process_data("output_exp22_2_5116563.dat")  
@@ -52,3 +87,5 @@ print(df_cb.to_string(index=False))
 
 print("\nResults for cs (student):")
 print(df_cs.to_string(index=False))
+
+"""
